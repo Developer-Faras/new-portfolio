9e9311391989a721ce8afce62ef18cb478caa863
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-10-19 10:19:42 +0600

    Fix Some Design And Add Favicon

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 $(window).on('load', function () {
     setTimeout(function () {
         $('.loader').addClass('loaded');
-    }, 2000);
+    }, 1000);
 
     setTimeout(function () {
         $('.loader').remove();
-    }, 3000);
+    }, 1500);
 });
 
-
+// All Jquery Event
 $(document).ready(function () {
     // Nav Toggler
     $('.nav-toggler').click(function () {
@@ -111,6 +111,20 @@ $(document).ready(function () {
 
     // Remove Active Fixed Cmponents To Scroll
     $('.main-content').scroll(function () {
+        if ($('.theme_color_wraper').hasClass('active')) {
+            $('.theme_color_wraper').removeClass('active');
+            $('.theme_changer_toggler').removeClass('active');
+        }
+
+        if ($('.sidebar').hasClass('mobile_active')) {
+            $('.nav-toggler').removeClass('mobile_active');
+            $('.sidebar').removeClass('mobile_active');
+            $('.fixed_content').removeClass('nav_active');
+        }
+    });
+
+    // Remove Active Fixed Main Content Click
+    $('.main-content').click(function () {
         if ($('.theme_color_wraper').hasClass('active')) {
             $('.theme_color_wraper').removeClass('active');
             $('.theme_changer_toggler').removeClass('active');
@@ -227,24 +241,24 @@ var swiper = new Swiper(".mySwiper", {
 
 
 // // Components Animation
-// AOS.init({
-//     // Global settings:
-//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-//     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-//     // initClassName: 'aos-init', // class applied after initialization
-//     // animatedClassName: 'aos-animate', // class applied on animation
-//     // useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-//     // disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-//     // debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-//     // throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    // initClassName: 'aos-init', // class applied after initialization
+    // animatedClassName: 'aos-animate', // class applied on animation
+    // useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    // disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    // debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    // throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
 
-//     // // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-//     // offset: 120, // offset (in px) from the original trigger point
-//     // delay: 0, // values from 0 to 3000, with step 50ms
-//     // duration: 800, // values from 0 to 3000, with step 50ms
-//     easing: 'ease', // default easing for AOS animations
-//     once: false, // whether animation should happen only once - while scrolling down
-//     mirror: false, // whether elements should animate out while scrolling past them
-//     // anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-// });
+    // // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    // offset: 120, // offset (in px) from the original trigger point
+    // delay: 0, // values from 0 to 3000, with step 50ms
+    // duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    // anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
