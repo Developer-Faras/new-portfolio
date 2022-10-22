e090826ef9f3465d9ca33e66413f90437d05fb79
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-10-22 20:39:52 +0600

    Working Scroll Animation

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,3 +1,40 @@
-$(document).ready(function () {
+(function ($) {
+    "use strict";
+    $.fn.scrollAnimation = function (customConfig) {
 
-});
+        let config = $.extend({
+            deaclare: $('[scroll_animation]'),
+            animateClass: 'animeted',
+            ofset: 150,
+            mirror: false,
+        }, customConfig);
+
+        let { deaclare, animateClass, ofset, mirror } = config;
+        let element = $(this);
+
+        $(this).scroll(function () {
+            $(deaclare).each(function () {
+                let animationName = $(this).attr('scroll_anim');
+                let element_ofset = $(this).offset().top - $(window).height() + ofset;
+
+                // Set Animation Name
+                $(this).addClass(animationName);
+
+                // Toggle Animated Class
+                if (mirror == true) {
+                    if (element_ofset <= 0) {
+                        $(this).addClass(animateClass);
+                    } else {
+                        $(this).removeClass(animateClass);
+                    }
+                } else if (mirror == false) {
+                    if (element_ofset <= 0) {
+                        $(this).addClass(animateClass);
+                    }
+                }
+
+            })
+        })
+    }
+
+})(jQuery)
